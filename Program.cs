0c9da819e665164af2aac3445eb50c23d38acc1c
JimBeakIt <87563663+JimBeakIt@users.noreply.github.com>
2021-10-26 11:50:32 +0100

    acme

--- Program.cs
+++ Program.cs
@@ -1,15 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace AcmeAccountingSystems
-{
+
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+        Console.WriteLine("Welcome to Acme Accounting Systems");
+        Console.WriteLine("Remember, we're \"accounting\" on you!");
+        Console.ReadLine();
         }
     }
-}
+
